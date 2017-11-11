b7ac2d564cd9e1f1f030e8be08a4e5d5798387e5
Manoj Nama <manoj.nama@outlook.com>
2017-11-11 22:58:36 +0530

    Correctig schema inport for auth model

--- models/auth.model.js
+++ models/auth.model.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const { Schema } = mongoose.Schema;
+const { Schema } = mongoose;
 
 const AuthSchema = new Schema({
   token: { type: String, required: true },
