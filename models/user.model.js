9d0b2d8bd71ebcece9f906a189d488848563db98
Manoj Nama <manoj.nama@outlook.com>
2017-11-11 18:44:09 +0530

    Updating user model indexes

--- models/user.model.js
+++ models/user.model.js
@@ -2,18 +2,20 @@ const mongoose = require('mongoose'),
   Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  social: {
-    google: { type: String },
-    facebook: { type: String }
+  'social': {
+    'google': { 'type': String },
+    'facebook': { 'type': String }
   },
-  name: { type: String },
-  username: { type: String },
-  email: { type: String },
-  phone: { type: String },
-  picture: { type: String }
+  'name': { 'type': String },
+  'username': { 'type': String },
+  'email': { 'type': String },
+  'phone': { 'type': String },
+  'picture': { 'type': String }
 });
 
-UserSchema.index({ token: 1 });
+UserSchema.index({ 'email': 1 });
+UserSchema.index({ 'social.google': 1 });
+UserSchema.index({ 'social.facebook': 1 });
 const UserModel = mongoose.model('Auth', UserSchema);
 
 export default UserModel;
